1dbd74cb1541895e2bafeb4c1135cbf555d5eed4
dblank8 <dblank8@gatech.edu>
2023-03-20 23:59:06 -0400

    create movie app

---
+++ app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head(){
+    return (
+        <>
+            <title>My Next App</title>
+            <meta name="description" content="A description of my Next.js app"></meta>
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    )
+
+}
